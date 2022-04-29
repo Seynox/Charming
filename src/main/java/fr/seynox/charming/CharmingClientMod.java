4307c95032a0298f1419069a3e22062d098f8177
Seynox <seynoxthegentleman@gmail.com>
2022-04-29 22:56:29 +0200

    Configured fabric, Added Client initializer

---
+++ src/main/java/fr/seynox/charming/CharmingClientMod.java
@@ -0,0 +1,12 @@
+package fr.seynox.charming;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CharmingClientMod implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        // TODO
+    }
+
+}
